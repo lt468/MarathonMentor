3f664c84743b913b6a66b6873dce49b39b435b65
lt468 <lucatremblayuk@gmail.com>
2023-10-20 14:50:50 +0100

    updated admin.py

--- training_plan/admin.py
+++ training_plan/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import RunnerUser, Run, MarathonPlan, ScheduledRun, TrainingWeek
+from .models import RunnerUser, MarathonPlan, ScheduledRun, CompletedRun
 
 # Register your models here.
 admin.site.register(RunnerUser)
-admin.site.register(Run)
 admin.site.register(MarathonPlan)
+admin.site.register(CompletedRun)
 admin.site.register(ScheduledRun)
-admin.site.register(TrainingWeek)
