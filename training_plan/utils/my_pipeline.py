cd5f55bb755dd2c8977b39f08a8f7414d0420f18
lt468 <lucatremblayuk@gmail.com>
2023-11-04 14:06:13 +0000

    able to grab the strava activities from the user

--- training_plan/utils/my_pipeline.py
+++ training_plan/utils/my_pipeline.py
@@ -1,4 +1,7 @@
-from ..models import StravaUserProfile
+import requests
+import urllib3
+from decouple import config
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def save_profile(backend, user, response, *args, **kwargs):
     try:
@@ -8,8 +11,40 @@ def save_profile(backend, user, response, *args, **kwargs):
         expires_at = response["expires_at"]
         firstname = response["athlete"]["firstname"]
         lastname = response["athlete"]["lastname"]
-        print(client_id, firstname, lastname, access_token)
-    except Exception as e:
-        print(e, e.args)
+        print("User Details:")
+        print(f"Client ID: {client_id}")
+        print(f"Access Token: {access_token}")
+        print(f"Refresh Token: {refresh_token}")
+        print(f"Expires At: {expires_at}")
+        print(f"First Name: {firstname}")
+        print(f"Last Name: {lastname}")
+        auth_url = "https://www.strava.com/api/v3/oauth/token"
+        activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
+        # Strava API credentials
+        client_id = config("STRAVA_CLIENT_ID")
+        client_secret = config("STRAVA_CLIENT_SECRET")
+        refresh_token = config("STRAVA_REFRESH_TOKEN")
+
+        payload = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': refresh_token,
+            'grant_type': "refresh_token",
+            'f': 'json'
+        }
+
+        print("Requesting Token...\n")
+        res = requests.post(auth_url, data=payload, verify=False)
+        #access_token = res.json()['access_token']
+        print("Access Token = {}\n".format(access_token))
+
+        header = {'Authorization': 'Bearer ' + access_token} # Need to use the access token for the user you want to get the runs on 
+        param = {'per_page': 1, 'page': 1}
+        my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+        print("Activity Details:")
+        print(my_dataset)
+    except Exception as e:
+        print("Error while retrieving user details:", e)
 
--- training_plan/views.py
+++ training_plan/views.py
@@ -17,38 +17,6 @@ def settings(request):
     return render(request, "training_plan/settings.html")
 
 @login_required
-@psa("social:complete")
-def strava_oauth_callback(request):
-
-    print("Called back!")
-    # Extract the query parameters from the URL
-    query_parameters = urlparse(request.get_full_path()).query
-    query_parameters = parse_qs(query_parameters)
-
-    # Extract the access token and refresh token
-    access_token = query_parameters.get('access_token', [None])[0]
-    refresh_token = query_parameters.get('refresh_token', [None])[0]
-    expires_at = query_parameters.get('expires_at', [None])[0]
-
-    # Get the current user
-    user = request.user
-
-    # Check if a StravaUserProfile already exists for this user
-    try:
-        strava_profile = StravaUserProfile.objects.get(user=user)
-        # Update the existing profile
-        strava_profile.strava_access_token = access_token
-        strava_profile.strava_refresh_token = refresh_token
-        strava_profile.expires_at = expires_at
-        strava_profile.save()
-    except StravaUserProfile.DoesNotExist:
-        # Create a new StravaUserProfile
-        strava_profile = StravaUserProfile(user=user, strava_access_token=access_token, strava_refresh_token=refresh_token, expires_at=expires_at)
-        strava_profile.save()
-
-    return HttpResponseRedirect(reverse("index"))
-
-@login_required
 def scheduled_runs(request):
     return render(request, "training_plan/scheduled_runs.html")
 
