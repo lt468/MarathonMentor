55022ff5eac4a56f1acefe8446292c6186ad506c
lt468 <lucatremblayuk@gmail.com>
2023-11-07 07:46:12 +0000

    Improved UI/UX for the mark as complete button on index page

--- training_plan/static/js/index.js
+++ training_plan/static/js/index.js
@@ -12,10 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
         changeBackground(box, upCommingRunsDictIds[i]);
     }
 
-    // Render button to the DOM
-    const rootMarkAsCompletedDiv = document.getElementById('root-mark-complete'); // Get the root div for the page
-    btnMarkAsCompleted = displayButton();
-    rootMarkAsCompletedDiv.appendChild(btnMarkAsCompleted);
+    // Get the document values from
+    const values = isRunDistance()
+
+    // Render button and label to the DOM (not if a rest day)
+    if (values.distance || values.sets) {
+        const rootMarkAsCompletedDiv = document.getElementById('root-mark-complete');
+        const buttonWithLabel = displayButton();
+        rootMarkAsCompletedDiv.appendChild(buttonWithLabel);
+    }
+
+    // Render info bar to the dom
+    const todaysRunDiv = document.getElementById('todays-run');
+    const infoBar = displayRunInfoBar(values);
+    todaysRunDiv.appendChild(infoBar);
+
 });
 
 function changeBackground(runBox, dictId) {
@@ -23,15 +34,86 @@ function changeBackground(runBox, dictId) {
     runBox.classList.add(`bg-grad-${dictId}`);
 }
 
-// Mark as complete button for the index page
+// Mark as complete button and label for the index page
 function displayButton() {
     let message = 'Mark As Completed';
 
-    let a = document.createElement('a');
-    a.id = `mark-complete`;
-    a.classList = 'btn btn-dark';
-    a.href = markAsCompletedURL;
-    a.role = 'button';
-    a.innerHTML = `${message}`;
-    return a;
+    // Create the button element
+    let button = document.createElement('a');
+    button.id = `mark-complete`;
+    button.classList = 'btn btn-dark';
+    button.href = markAsCompletedURL;
+    button.role = 'button';
+    button.innerHTML = `${message}`;
+
+    // Create the label element
+    let label = document.createElement('small');
+    label.classList = 'text-muted me-2';
+    label.innerText = 'Mark as complete to enter your stats';
+
+    // Create a div to hold the button and label
+    let container = document.createElement('div');
+    container.classList = 'd-flex align-items-center';
+    container.appendChild(label);
+    container.appendChild(button);
+
+    return container;
 }
+
+function displayRunInfoBar(values) {
+
+    // Check to see if the run is a distance or interval based run
+    let type;
+    if (values['distance']) {
+        type = 'distance';
+    } else if (values['sets']) {
+        type = 'interval';
+    } else {
+        type = 'rest';
+    }
+
+    let rootDiv = document.createElement('div');
+    rootDiv.id = 'run-info-bar'
+    rootDiv.classList ='m-1 hstack gap-3 fs-5'
+
+    // I need to make it dynamic so I change both the value labels and the values!
+    if (type === 'distance') {
+        rootDiv.innerHTML = `
+            <div id="info-bar--distance" class="m-auto">Distance: ${values.distance}km</div>
+            <div class="vr"></div>
+            <div id="infor-bar--duration" class="m-auto">Estimated Duration: ${values.est_duration} minutes</div>
+            <div class="vr"></div>
+            <div id="infor-bar--pace" class="m-auto">Estimated Pace: ${formatTime(values.est_avg_pace)}</div>
+        `
+    } else if (type === 'interval') {
+        rootDiv.innerHTML = `
+            <div id="info-bar--time" class="m-auto">Working time: ${values.on}km</div>
+            <div class="vr"></div>
+            <div id="infor-bar--duration" class="m-auto">Rest time: ${values.off} minutes</div>
+            <div class="vr"></div>
+            <div id="infor-bar--pace" class="m-auto">Sets: ${values.sets}</div>
+            <div class="vr"></div>
+            <div id="infor-bar--pace" class="m-auto">Estimated Pace: ${formatTime(values.est_avg_pace)}</div>
+        `
+    } else {
+        rootDiv.innerHTML = `
+            <div id="infor-bar--rest" class="fs-5">Enjoy your rest day</div>
+        `
+    }
+    return rootDiv;
+}
+
+function formatTime(timeString) {
+    // Split the time string
+    const timeParts = timeString.split(':');
+    
+    // Extract minutes and seconds
+    const minutes = timeParts[1];
+    const seconds = Math.round(parseFloat(timeParts[2]));
+    
+    // Format minutes and seconds with leading zeros
+    const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    
+    return formattedTime;
+}
+
